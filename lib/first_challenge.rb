dcdd9d41f5e9c677767192eb2ab0946a2fe2e921
Marcel Rico <personal.mr4@gmail.com>
2019-11-25 22:28:41 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,4 +1,4 @@
 epic_tragedy = {
   :montague => {:matriarch=>"Lady Montague",:patriarch=>"Lord Montague"},
-  :capulet => {:matriarch=>"Lady Capulet",:patriarch=>"Lord Capulet"}
+  :capulet => {:matriarch=}
 }
